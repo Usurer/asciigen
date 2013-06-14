79ee0ad291b4a3b2e44198257642a1179d8a745a
Usurer <ivvlg@narod.ru>
2013-06-15 03:47:34 +0400

    Algorithm of pixels to symbol converson was improved. Actually it's just a piece of shitty code, but it does whhat I want it to do.

--- image.py
+++ image.py
@@ -1,21 +1,12 @@
-#-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
-#
-# Author:      Usurer
-#
-# Created:     12/06/2013
-# Copyright:   (c) Usurer 2013
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
 import sys
 from PIL import Image, ImageFont, ImageDraw, ImageWin
+
 
 def main():
      #An image to convert to ascii
     bwGoat = Image.open("goat.jpg")
     #Making it black-n-white
-    bwGoat = bwGoat.convert('L');
+    bwGoat = bwGoat.convert('L')
 ##    bwGoat.save("bwGoat.jpg")
 
     sd = getSymbolsDensityArray()
@@ -25,7 +16,7 @@ def main():
     h = bwGoat.size[1]
 
     imageDensity = []
-    #Goat image is 600x400. I use 2x1 pieces as a single slice to be changed whith ascii symbol
+    #Goat image is 600x400. I use 2x1 pieces as a single slice to be changed with ascii symbol
     for row in range(0, 199):
         densityRow = []
         for col in range(0, 599):
@@ -38,31 +29,40 @@ def main():
     #Now I want to get a 'step', a level of discretization for image pixels
     #To make convertion better I should convert 'em to ascii in accordance
     #with ascii symbols' density distribution
+
+    discreteArray = getDiscreteArray(sd[1])
+    print discreteArray
+
     allVals = []
     for row in imageDensity:
         for col in row:
             allVals.append(col)
     minMax = getMaxMin(allVals)
     deltaRange = minMax[1] - minMax[0]
-    delta = deltaRange / 26
-##    print minMax, delta
+    delta = float(deltaRange) / len(discreteArray)
+    print 'Delta is ' + str(delta)
 
     normalizedDensities = []
     for row in imageDensity:
         tempRow = []
         for col in row:
-            tmp = (col - minMax[0]) // delta
+            tmp = int((col - minMax[0]) // delta)
             tempRow.append(tmp)
-            ##sys.stdout.write(str(tmp) + ' ')
         normalizedDensities.append(tempRow)
-        ##sys.stdout.write('\n')
 
-    f = open('result.txt', 'w')
+    print sd[0]
+    print sd[1]
+
+    f = open('result_2.txt', 'w')
     for row in normalizedDensities:
         for col in row:
-            f.write(sd[0][col-1])
+            symb = getSymbolByDensity(sd, discreteArray[col])
+            f.write(symb)
         f.write('\n')
+
+    print 'Conversion finished!'
     pass
+
 
 #Returns an array of [[symbols],[densities] sorted by density
 def getSymbolsDensityArray():
@@ -90,6 +90,16 @@ def getSymbolsDensityArray():
     return [symbols, densities]
 
 
+#Returns a symbol of known density from [symbols, densities] type of array.
+def getSymbolByDensity(symbolDensityArray, density):
+    result = None
+    for i in range(0, len(symbolDensityArray[1])):
+        if symbolDensityArray[1][i] == density:
+            result = symbolDensityArray[0][i]
+            break
+    return result
+
+
 #A sum of all pixels' values. White is 255, black is zero.
 #While we print with black symbols, now white, this weight should be inverted later
 def calculateSymbolWeight(image):
@@ -98,6 +108,7 @@ def calculateSymbolWeight(image):
         for j in range(0, 19):
             val = val + image.getpixel((i, j))
     return val
+
 
 #A weight of all pixels in a piece of an image
 def calculateSliceWeight(pixArr, sliceTuple):
@@ -112,6 +123,7 @@ def calculateSliceWeight(pixArr, sliceTuple):
 ##    sys.stdout.write(str(pixArr[300, 300]))
     return res
 
+
 #[min, max] values of array
 def getMaxMin(arr):
     min = arr[0]
@@ -122,6 +134,34 @@ def getMaxMin(arr):
         if a < min:
             min = a
     return [min, max]
+
+
+def getMinDeltaOfArray(arr):
+    delta = abs(arr[0] - arr[1])
+    for i in arr:
+        for j in arr:
+            if i != j:
+                if abs(i - j) < delta:
+                    delta = abs(i - j)
+    return delta
+
+
+#Used to convert array of symbol densities to array of ranges.
+#For example if we have [1, 3, 10], the result should be [1, 1, 3, 3, 3, 3, 10] or... well.. kinda like that )
+def getDiscreteArray(arr):
+    delta = getMinDeltaOfArray(arr)
+    minVal = getMaxMin(arr)[0]
+    maxVal = getMaxMin(arr)[1]
+    arrayRange = maxVal - minVal
+    cellsAmount = arrayRange // delta + 1  # It should be Math.Ceil here, but I don't want import it yet
+    tmpArr = []
+    arrayIterator = 0
+    for i in range(0, cellsAmount):
+        if (minVal + delta * i) > arr[arrayIterator]:
+            arrayIterator += 1
+        tmpArr.append(arr[arrayIterator])
+    return tmpArr
+
 
 if __name__ == '__main__':
     main()
